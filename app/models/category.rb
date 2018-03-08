3f328a9a8b50108a42424557b8c98e243d2920ba
Raghu Betina <raghu@firstdraft.com>
2018-03-08 19:26:12 +0000

    Direct association between categories and itineraries

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,9 @@
 class Category < ApplicationRecord
   # Direct associations
 
+  has_many   :itineraries,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,6 +1,8 @@
 class Itinerary < ApplicationRecord
   # Direct associations
 
+  belongs_to :category
+
   has_many   :comments,
              :dependent => :destroy
 
