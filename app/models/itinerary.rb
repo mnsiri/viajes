233b606a5efd75d12d38ecbe8b585ce996477603
Raghu Betina <raghu@firstdraft.com>
2018-03-08 19:26:12 +0000

    Direct association between itineraries and comments

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,6 +1,9 @@
 class Itinerary < ApplicationRecord
   # Direct associations
 
+  has_many   :comments,
+             :dependent => :destroy
+
   has_many   :likes,
              :dependent => :destroy
 
