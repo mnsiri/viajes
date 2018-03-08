77e18d566913463cc6ab9a30a01c732114352cc8
Raghu Betina <raghu@firstdraft.com>
2018-03-08 19:26:51 +0000

    Whitelist columns in Itinerary for ActiveAdmin

--- app/admin/itineraries.rb
+++ app/admin/itineraries.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Itinerary do
+
+ permit_params :user_id, :country, :category_id, :name
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
